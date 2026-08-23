68c6452a07fd6a1511a69bf2310ebb705fe7b8b6
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 17:17:50 +0200

    chore(preview): publish expanded apartment hub

--- preview-v2/src/v2/app.js
+++ preview-v2/src/v2/app.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@1aa6731c166601e9b47ea0ebbdb7e78a800057f1/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@1c9d621609522200f86b2b30957074514941eb4f/src/v2/app.js';
