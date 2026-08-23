c45800bfecfe80bf83668cfe23345389fc24c541
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 17:51:05 +0200

    chore(preview): publish Tobi story mission

--- preview-v2/src/v2/app-hubflow091.js
+++ preview-v2/src/v2/app-hubflow091.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@40bb575c42074ea2e05f8b83b1c1ada9ebd1e6ac/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@2d51bfdb3beecd56e8781343595a0eee1bc9e733/src/v2/app.js';
