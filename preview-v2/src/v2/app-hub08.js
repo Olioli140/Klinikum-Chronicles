b88ba423554292c7ab661acffd454baf72cda752
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 17:33:22 +0200

    chore(preview): publish mission board and phone missions

--- preview-v2/src/v2/app-hub08.js
+++ preview-v2/src/v2/app-hub08.js
@@ -1,1 +1,2 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@1c9d621609522200f86b2b30957074514941eb4f/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@131ed8d72b121b45bfaa279665e0647bd02c4381/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@131ed8d72b121b45bfaa279665e0647bd02c4381/src/v2/mission-hub.js';
