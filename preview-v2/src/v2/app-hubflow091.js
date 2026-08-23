78e38f1de481f6bdfa16073f98eaa1876369aa49
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 19:43:17 +0200

    Update v2 preview to latest first-shift dialogue build

--- preview-v2/src/v2/app-hubflow091.js
+++ preview-v2/src/v2/app-hubflow091.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@5931be96d427576182005e926f2a1edd7b0c2093/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@64b624bdfa4669976e18dd81fe10d6b803c2f34c/src/v2/app.js';
