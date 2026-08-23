687b25d74f65b3e8b8702bd8df6fdb81ea9285c6
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 19:39:41 +0200

    Point v2 preview at latest development snapshot

--- preview-v2/src/v2/app-hubflow091.js
+++ preview-v2/src/v2/app-hubflow091.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@1b59b67b08678c88716c67c04a6676d2a303f91f/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@5931be96d427576182005e926f2a1edd7b0c2093/src/v2/app.js';
