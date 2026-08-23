ff625eed503ed9a9bf970e135368d9c5d1aca0af
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 16:36:48 +0200

    preview(v2): pin latest party-builder build

--- preview-v2/src/v2/app.js
+++ preview-v2/src/v2/app.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@feature/roguelite-v2/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@25f2a65253bad78cea5d22c9cb2e51a95c257e7f/src/v2/app.js';
