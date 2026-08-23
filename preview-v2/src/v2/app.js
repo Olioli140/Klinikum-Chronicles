27f7645503547275ba5f07ad3b82b54b051129de
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-23 16:50:48 +0200

    chore(preview): point v2 preview to equipment build

--- preview-v2/src/v2/app.js
+++ preview-v2/src/v2/app.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@2849e156b604d0a4cac4e98270c3d475c2da903e/src/v2/app.js';
+import 'https://cdn.jsdelivr.net/gh/Olioli140/Klinikum-Chronicles@b45bb428447c837cec9b01eeb10efb23ce128d78/src/v2/app.js';
